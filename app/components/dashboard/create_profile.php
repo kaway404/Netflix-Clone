9085b906f72e3787b70918298c1a5a67494c1f78
Alexandre Silva <xande1231231@hotmail.com>
2018-06-17 10:29:34 -0300

    Fix href

--- app/components/dashboard/create_profile.php
+++ app/components/dashboard/create_profile.php
@@ -29,8 +29,8 @@
 		<center>
 		<button class="gerenc" id="createprofile" style="top: 60px; left: 20px">Criar</button>
 	</form>
-	<button style="color: #333; background: #fff; top: 60px; left: 20px" class="gerenc">Voltar</button>
-	<div id="message"></div>
+	<a href="/browse"><button style="color: #333; background: #fff; top: 60px; left: 20px" class="gerenc">Voltar</button>
+	<div id="message"></div></a>
 </center>
 <br>
 <br>
